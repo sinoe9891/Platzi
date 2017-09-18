63328756ba3f0c976b43b031155b3ff7f794df01
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2017-09-18 08:33:01 -0600

    screen

--- programación Basica/ciclos_while_for/dibujo.js
+++ programación Basica/ciclos_while_for/dibujo.js
@@ -4,25 +4,34 @@ var lineas = 30;
 var l = 0;
 var yi, xf;
 var xf1;
+var tamañow, tamañoh;
+tamañow = screen.height;
+tamañoh = screen.height;
+d.setAttribute("height", tamañoh);
+d.setAttribute("width", tamañow);
+
 
 for (var l = 0; l < lineas; l++) {
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  xf1 = 300 - (l * 10);
+  yi = 24 * l;
+  xf = 24 * (l + 1);
+  xf1 = tamañow - (l * 24);
   // Inf-Izquierda
-  dibujarLinea("#981818", 0, yi, xf, 300);
+  dibujarLinea("#981818", 0, yi, xf, tamañoh);
   // Inf-Derecha
-  dibujarLinea("#981818", 300, yi, xf1, 300);
+  dibujarLinea("#981818", tamañow, yi, xf1, tamañoh);
    // Sup-Izquierda
   dibujarLinea("#981818", xf1, 0, 0, xf);
   // Sup-Derecha
-  dibujarLinea("#981818", 300, xf1, xf1, 0);
+  dibujarLinea("#981818", tamañow, xf, yi, 0);
 }
-
-dibujarLinea("#981818", 1,1,1,300);
-dibujarLinea("#981818", 1,300,300,300);
-dibujarLinea("#981818", 300,0,300,300);
-dibujarLinea("#981818", 300,00,0,0);
+//Linéa Izquirda
+dibujarLinea("#981818", 1,1,1,tamañoh);
+//Linéa abajo
+dibujarLinea("#981818", 1,tamañoh,tamañow,tamañoh);
+//Linéa derecha
+dibujarLinea("#981818", tamañow,0,tamañow,tamañoh);
+//Linea arriba
+dibujarLinea("#981818", tamañow,0,0,0);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
