1f86f3422ea7d9b8ee8d9a3b195af26886f4867e
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2017-12-28 01:03:11 -0600

    Arrays

---
+++ Curso PHP/variables/arrays.php
@@ -0,0 +1,18 @@
+<?php
+
+$arrayVar = [
+							'color1' => 'red',
+							'color2' => 'azul',
+							3 => 'black',
+						];
+
+var_dump($arrayVar['color2'] . "</br>");
+
+$array1Var = [
+	0 => 'red' . "</br>",
+	1 =>	'blue' . "</br>",
+	2 =>	'black' . "</br>",
+];
+
+var_dump($array1Var);
+?>
--- Curso PHP/variables/strings.php
+++ Curso PHP/variables/strings.php
@@ -1,27 +1,8 @@
 <?php
-//Cadenas de Caractéres
-//int
-$intVar = 1;
-var_dump($intVar);
 
-// float
-$floatVar = 1.2;
-var_dump($floatVar);
 
-// string
-$stringVar = 'Sample Text';
-var_dump($stringVar);
-
-// bool
-$boolVar = true;
-var_dump($boolVar);
-
-// null
-$nullVar = null;
-var_dump($nullVar);
-
-$arrayVar = ['valor1', 12, 1.3];
-var_dump($arrayVar);
-
+$intVar = 5;
+$stringVar = "Hello " . $intVar;
+echo $stringVar;
 
 ?>
