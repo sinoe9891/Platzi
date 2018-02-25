f6eb363be1bb359f8e1206e123de884c11fed2f1
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2018-02-24 23:32:29 -0600

    php ! atom

--- Curso PHP/hello-world/index.php
+++ Curso PHP/hello-world/index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>My first script</title>
-	</head>
-	<body>
-		<?php
-			echo "Hello World";
-
-		?>
-
-	</body>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Mi Primer Script</title>
+</head>
+<body>
+	<?php
+		echo "Hola Mundo";
+	?>
+</body>
 </html>
