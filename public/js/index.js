c69e72437a9c06769fbe6cfaa450bde83b7a2aa3
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2018-11-19 13:57:27 -0600

    Js

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+firebase.auth().onAuthStateChanged(function(user){
+  // if (user) {
+  //   console.log(user);
+  //   //nombre del usuario
+  //   console.log(user.displayName);
+  // }else {
+  //   console.log("no tenemos usuario");
+  // }
+});
