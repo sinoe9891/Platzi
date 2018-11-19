452fd684e0d9ed66a069c1c693bbf4b12479bf01
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2018-11-19 15:55:08 -0600

    clase 3

---
+++ Desarrollador de JavaScript/Fundamentos de JavaScript/clase3.js
@@ -0,0 +1,50 @@
+var edad = 27
+
+// Incremento
+// edad = edad + 1
+edad += 1
+
+var peso = 75
+
+// peso = peso - 2
+
+peso -= 2
+
+// sumar
+
+var sandwich = 1
+
+peso = peso + sandwich
+
+// restar
+var jugarFutbol = 3
+
+peso = peso - jugarFutbol
+
+// Decimales
+
+var preciovino = 200.3
+
+var total = preciovino * 3
+
+// reucir decimales
+
+total = preciovino * 100 * 3 / 100
+
+// modulo matematico
+
+var total = Math.round(preciovino * 100 * 3) / 100
+
+// aumentar los decimales pero en strings
+var totalStr = total.toFixed(2)
+
+// pasar a numero
+var total2 = parseFloat(totalStr)
+
+// division
+var pizza = 8
+var personas = 2
+
+var cantidadporcionesporpersona = pizza / personas
+
+console.log('Tocan ' + `${cantidadporcionesporpersona}` + ' por persona')
