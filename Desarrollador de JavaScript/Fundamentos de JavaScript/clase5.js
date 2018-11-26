7a428724817c7792749ef2990f371e32fba66436
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2018-11-26 08:19:05 -0600

    final clase 5

---
+++ Desarrollador de JavaScript/Fundamentos de JavaScript/clase5.js
@@ -0,0 +1,28 @@
+var nombre = 'Danny'
+
+function imprimirMayuscula() {
+    nombre = nombre.toUpperCase()
+        //console.log(nombre)
+    document.getElementById("boton").innerHTML = nombre.toUpperCase();
+}
+// imprimirMayuscula()
+
+
+// var nombre = 'Danny'
+
+// function imprimirMayuscula(nombre) {
+//     nombre = nombre.toUpperCase()
+//     console.log(nombre)
+// }
+// imprimirMayuscula(nombre)
+
+
+
+// var nombre = 'Danny'
+
+// function imprimirMayuscula(n) {
+//     n = nombre.toUpperCase()
+//     console.log(nombre)
+//         // document.getElementById("boton").innerHTML = nombre.toUpperCase();
+// }
+// imprimirMayuscula(nombre)
