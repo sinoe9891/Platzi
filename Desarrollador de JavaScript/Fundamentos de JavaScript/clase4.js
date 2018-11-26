ad579d5bf4016c027b521f028144334efecd41a5
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2018-11-25 23:12:32 -0600

    Clase 4

---
+++ Desarrollador de JavaScript/Fundamentos de JavaScript/clase4.js
@@ -0,0 +1,20 @@
+// var nombre = 'Danny',
+//     edad = '28'
+
+// function imprimirEdad() {
+//     console.log(`${nombre} tiene ${edad} años`)
+// }
+
+// imprimirEdad()
+
+var nombre = 'Danny',
+    edad = '28'
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Eric', 24)
+imprimirEdad('Darío', 27)
