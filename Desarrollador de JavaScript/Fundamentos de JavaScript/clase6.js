c8c75d2de1ab23f5832d1cdc798e383e232ffa59
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2018-11-30 15:57:25 -0600

    objetos

--- Desarrollador de JavaScript/Fundamentos de JavaScript/clase6.js
+++ Desarrollador de JavaScript/Fundamentos de JavaScript/clase6.js
@@ -9,6 +9,29 @@
 // imprimirMayuscula(nombredanny)
 // imprimirMayuscula(nombreleo)
 // imprimirMayuscula(nombrelaura)
+////////------------Ejemplo 1-------------
+// var danny = {
+//     nombre: 'Danny',
+//     apellido: 'Velasquez',
+//     edad: 28
+// }
+
+// var Leo = {
+//     nombre: 'Leonardo',
+//     apellido: 'Rodriguez',
+//     edad: 24
+// }
+
+// function imprimirMayuscula(nombre) {
+//     nombre = nombre.toUpperCase()
+//     console.log(nombre)
+// }
+
+// imprimirMayuscula(danny.nombre)
+// imprimirMayuscula(Leo.nombre)
+
+
+////////------------Ejemplo 2-------------
 
 var danny = {
     nombre: 'Danny',
@@ -22,10 +45,10 @@ var Leo = {
     edad: 24
 }
 
-function imprimirMayuscula(nombre) {
-    nombre = nombre.toUpperCase()
+function imprimirMayuscula(persona) {
+    var nombre = persona.nombre.toUpperCase()
     console.log(nombre)
 }
 
-imprimirMayuscula(danny.nombre)
-imprimirMayuscula(Leo.nombre)
+imprimirMayuscula(danny)
+imprimirMayuscula(Leo)
