387af9dac45de70359778bb6d23f5ff612b05f4f
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2019-01-08 09:40:12 -0600

    repasar clase 1

--- Desarrollador de JavaScript/Fundamentos de JavaScript/clase1.js
+++ Desarrollador de JavaScript/Fundamentos de JavaScript/clase1.js
@@ -1,23 +1,34 @@
+// Ejemplo 1
 // console.log('Hola Mundo JS!')
-
 // var nombre
-
 // nombre = 'Danny'
+// console.log('Hola ' + nombre)
 
+// Ejemplo 2
 // var nombre = 'Danny'
 // var apellido = 'Velásquez'
+// console.log('Hola ' + nombre + ' ' + apellido)
 
+// Ejemplo 3
+// var nombre = 'Danny',
+//     apellido = 'Velásquez'
+// console.log('Hola ' + nombre + ' ' + apellido)
+
+// Ejemplo 4
+// var nombre = 'Danny',
+//     apellido = 'Velásquez',
+//     edad = 28
+// console.log('Hola mi nombre es ' + nombre + ' ' + apellido + ' y tengo ' + edad + ' años')
+
+// Ejemplo5
+// puede usarse así pero sería un String
 var nombre = 'Danny',
     apellido = 'Velásquez'
+var edad = '28 años'
+console.log('Hola mi nombre es ' + nombre + ' ' + apellido + ' y tengo ' + edad)
 
-var edad = 28
-
-edad = '28 años'
-    // console.log('Hola ' + nombre)
-console.log('Hola ' + nombre + ' ' + apellido)
-
-console.log('Tengo ' + edad + ' años')
-
-console.log('Tengo ' + edad)
-
-var peso = 75
+// Ejemplo 6
+// podemos llamar la variable pero se puede cambiar aunque ya este declarada
+// var peso = 75
+// peso = "75kg"
+// Pero ya no sería un numero si no un  String
