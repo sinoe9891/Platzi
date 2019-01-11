fa7e3c1124f9a5ff0ad9c76f1c0893ab0fd6012b
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2019-01-11 16:03:49 -0600

    clase8

--- Desarrollador de JavaScript/Fundamentos de JavaScript/clase8.js
+++ Desarrollador de JavaScript/Fundamentos de JavaScript/clase8.js
@@ -1,20 +1,17 @@
 var danny = {
-    // nombre es key o llave y danny es valor
     nombre: 'Danny',
     apellido: 'Velasquez',
-    edad: 29.5,
+    edad: 29,
     universidad: 'ZAMORANO'
 }
 
-
-imprimirNombreInformarcion(danny)
-    //////------------Ejercicio1-------------
 function imprimirNombreYEdad(varLocal) {
     var { nombre, edad, universidad } = varLocal
-    console.log(nombre, edad)
-        // console.log(nombre.toUpperCase(), edad.toFixed(0))
-        // console.log(`Ejemplo2- Hola, me llamo ${nombre.toUpperCase()} y tengo ${edad} años y trabajo en la Escuela Agrícola Panamericana ${universidad.toUpperCase()}`)
+    console.log(nombre, edad, universidad)
 }
 imprimirNombreYEdad(danny)
 
-// Reto
+function cumpleanos(varLocal) {
+    varLocal.edad += 1
+}
+// cumpleanos()
