12b00c44f746cebf42bee2c2ccafa5efc9ddf5b2
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2019-01-16 16:09:25 -0600

    clase8

--- Desarrollador de JavaScript/Fundamentos de JavaScript/clase8.js
+++ Desarrollador de JavaScript/Fundamentos de JavaScript/clase8.js
@@ -9,12 +9,14 @@ function imprimirNombreYEdad(varLocal) {
     var { nombre, edad, universidad } = varLocal
     console.log(nombre, edad, universidad)
 }
-imprimirNombreYEdad(danny)
-    // Ejemplo 2
-    // function cumpleanos(varLocal) {
-    //     varLocal.edad += 1
-    // }
-    // cumpleanos()
+
+console.log(imprimirNombreYEdad(danny))
+
+// Ejemplo 2
+// function cumpleanos(varLocal) {
+//     varLocal.edad += 1
+// }
+// cumpleanos()
 
 // si observamos que cuando llamamos al objeto danny tien 29
 // pero si llamamos la funcion veremos reflejado que se sumó uno
@@ -29,14 +31,43 @@ function cumpleanos(varLocal) {
         edad: varLocal.edad + 1
     }
 }
-// Ejemplos en consola
-// cumpleanos(danny)
-// {nombre: "Danny", apellido: "Velasquez", edad: 30, universidad: "ZAMORANO"}
-// Resultado: danny
-// {nombre: "Danny", apellido: "Velasquez", edad: 29, universidad: "ZAMORANO"}
-// var dannyViejo = cumpleanos(danny)
-// Resultado: undefined
-// dannyViejo
-// Resultado: {nombre: "Danny", apellido: "Velasquez", edad: 30, universidad: "ZAMORANO"}
-// danny
-// Resultado: {nombre: "Danny", apellido: "Velasquez", edad: 29, universidad: "ZAMORANO"}
+console.log(cumpleanos(danny))
+    // Ejemplos en consola
+    // cumpleanos(danny)
+    // {nombre: "Danny", apellido: "Velasquez", edad: 30, universidad: "ZAMORANO"}
+    // Resultado: danny
+    // {nombre: "Danny", apellido: "Velasquez", edad: 29, universidad: "ZAMORANO"}
+    // var dannyViejo = cumpleanos(danny)
+    // Resultado: undefined
+    // dannyViejo
+    // Resultado: {nombre: "Danny", apellido: "Velasquez", edad: 30, universidad: "ZAMORANO"}
+    // danny
+    // Resultado: {nombre: "Danny", apellido: "Velasquez", edad: 29, universidad: "ZAMORANO"}
+
+
+// Ejemplo 3
+
+// Sin Return
+var dividirPorCuatro1 = function(numero) {
+    numero / 4;
+}
+var dividirCantidad1 = dividirPorCuatro1(27)
+    // Imprimir
+console.log(dividirPorCuatro1(27))
+    // REsultado undefined
+
+// Con Return
+var dividirPorCuatro = function(numero) {
+    return numero / 4;
+}
+var dividirCantidad = dividirPorCuatro(27)
+
+// Imprimir
+console.log(dividirPorCuatro(27))
+    // Resultado 6.75
+
+// ejemplo 4
+function ejemplo(x) {
+    return x * x
+}
+console.log(ejemplo(7))
