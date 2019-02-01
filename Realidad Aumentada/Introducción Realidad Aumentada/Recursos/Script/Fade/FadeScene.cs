282d62c98998a45bd6bacfe24ca9c0bdd9b28e43
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2019-02-01 15:27:01 -0600

    Realidad Aumentada

---
+++ Realidad Aumentada/Introducción Realidad Aumentada/Recursos/Script/Fade/FadeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeScene : MonoBehaviour {
+
+    //name of the scene you want to load
+    public string scene;
+    public Color loadToColor = Color.white;
+    public object boton;
+    public float tiempo = 5f;
+
+    // Update is called once per frame
+    public void Fade()
+
+    {
+        //Button to load the new scene
+
+        Initiate.Fade(scene, loadToColor, tiempo);
+    }
+}
---
+++ Realidad Aumentada/Introducción Realidad Aumentada/Recursos/Script/escena.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class escena : MonoBehaviour {
+
+    public string nombre;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    public void cargarescena() {
+
+        SceneManager.LoadScene(nombre);
+
+    }
+}
