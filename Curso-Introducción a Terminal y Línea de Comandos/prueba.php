95d5b67e46b160105099b8d8df547c9f315a1552
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2019-05-06 07:26:11 -0600

    terminal

---
+++ Curso-Introducción a Terminal y Línea de Comandos/prueba.php
@@ -0,0 +1,15 @@
+<?php 
+echo "número: ";
+$d = trim(fread(STDIN, 100));
+$i = 0;
+
+while(true) {
+    if(++$i % $d == 0) {
+        fwrite(STDOUT, sprintf("El %d es múltiplo de %d.\n", $i, $d));
+    } else {
+        fwrite(STDERR,
+            sprintf("Error, El %d NO es múltiplo de %d \n", $i, $d));
+    }
+    sleep(1);
+}
+?>
